4115a26af6247b7afa13d98bdcf0c523bee120dd
SajalTalukder <76643944+SajalTalukder@users.noreply.github.com>
2021-01-02 06:16:27 -0600

    Update app.js

--- app.js
+++ app.js
@@ -4,3 +4,21 @@ navBurger.addEventListener("click", () => {
   navBurger.classList.toggle("nav__toggle");
   navUl.classList.toggle("nav__ul__open");
 });
+
+// scroll animation
+gsap.registerPlugin(ScrollTrigger);
+let t1 = gsap.timeline();
+t1.from(".animation__left", {
+  x: -300,
+  opacity: 0,
+  duration: 1.4,
+  stagger: 1,
+}).from(
+  ".animation__down",
+  {
+    y: 50,
+    opacity: 0,
+    duration: 1,
+  },
+  "-=0.8"
+);
