ce036607aadb953aad45979bfcb9105cd0f98c62
SajalTalukder <76643944+SajalTalukder@users.noreply.github.com>
2021-01-03 00:46:23 -0600

    using GSAP for animation

--- app.js
+++ app.js
@@ -5,20 +5,65 @@ navBurger.addEventListener("click", () => {
   navUl.classList.toggle("nav__ul__open");
 });
 
-// scroll animation
-gsap.registerPlugin(ScrollTrigger);
-let t1 = gsap.timeline();
-t1.from(".animation__left", {
-  x: -300,
+const top_bottom = document.querySelector(".top-bottom");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY >= 500) {
+    top_bottom.classList.add("top-bottom__show");
+  } else {
+    top_bottom.classList.remove("top-bottom__show");
+  }
+});
+// smoth scroll
+let scroll = new SmoothScroll('a[href*="#"]', {
+  speed: 800,
+});
+
+gsap.from(".section-1__box", {
+  scrollTrigger: ".section-1__box",
+  x: -1000,
+  duration: 1,
+  stagger: 0.2,
+});
+gsap.from(".composition__img--1", {
+  scrollTrigger: ".composition__img--1",
+  y: -100,
+  duration: 1,
   opacity: 0,
-  duration: 1.4,
-  stagger: 1,
-}).from(
-  ".animation__down",
-  {
-    y: 50,
-    opacity: 0,
-    duration: 1,
-  },
-  "-=0.8"
-);
+});
+gsap.from(".composition__img--2", {
+  scrollTrigger: ".composition__img--2",
+  x: 100,
+  duration: 1,
+  opacity: 0,
+});
+gsap.from(".composition__img--3", {
+  scrollTrigger: ".composition__img--2",
+  y: 100,
+  duration: 1,
+  opacity: 0,
+  delay: 0.3,
+});
+gsap.from(".section-2__text", {
+  scrollTrigger: ".section-2__text",
+  x: -500,
+  duration: 1,
+  delay: 0.5,
+});
+
+gsap.from(".work-card", {
+  scrollTrigger: ".work-card",
+  y: 400,
+  duration: 0.8,
+  stagger: 0.3,
+  opacity: 0,
+  delay: 0.4,
+});
+gsap.from(".card", {
+  scrollTrigger: ".card",
+  y: 400,
+  duration: 0.8,
+  stagger: 0.3,
+  opacity: 0,
+  delay: 0.4,
+});
